6e3ba2e45181b2efd44ae90c63b3850fe81bb14b
Bas de Haas <wbdehaas@xs4all.nl>
2012-08-31 12:33:07 +0200

    cleaned up and added documentation

--- HarmTrace/Base/Parsing.hs
+++ HarmTrace/Base/Parsing.hs
@@ -18,28 +18,29 @@ import Data.ListLike.Base (ListLike)
 -- A collection of parsing functions used by parsers throughout the project
 --------------------------------------------------------------------------------     
 
--- toplevel parsers
+-- | Toplevel parser that ignores error-reporting, regardless of there were
+-- error in the parse
 parseData :: (ListLike s a, Show a) => P (Str a s LineColPos) b -> s -> b
 parseData p inp = fst ( parseDataWithErrors p inp )
 
+-- | Toplevel parser that returns both the result as well as a (possibly empty)
+-- list of error-corrections.
 parseDataWithErrors :: (ListLike s a, Show a) 
                     =>  P (Str a s LineColPos) b -> s -> (b, [Error LineColPos])
 parseDataWithErrors p inp = (parse ( (,) <$> p <*> pEnd) 
                              (createStr (LineColPos 0 0 0) inp))
                                                  
--- parses specific string
+-- | Parses specific string
 pString :: (ListLike state a, IsLocationUpdatedBy loc a, Show a, Eq a) 
         => [a] -> P (Str a state loc) [a]
-pString s = foldr (\a b -> (:) <$> a <*> b) (pure []) (map pSym s)                             
 
--- parses whitespace (@pedro: should probably not contain '\n')
--- pSpaces :: Parser Char
--- pSpaces = pAnySym [' ','\n','\t']
-
--- parses UNIX and DOS/WINDOWS line endings
+-- | Parses UNIX and DOS/WINDOWS line endings
 pLineEnd :: Parser String
 pLineEnd  = pString "\n" <|> pString "\r\n" <|> pString " " <|> pString "\t"  
 
+-- | Parses an arbitrary times the first parsing combinator until the parsing 
+-- second parsing combinator is encountered. The result of the second parsing
+-- combinator is ignored.
 pManyTill :: P st a -> P st b -> P st [a]
 pManyTill p end = [] <$ end 
                   <<|> 
