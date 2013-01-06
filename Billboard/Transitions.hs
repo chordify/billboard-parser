69b9e212102fac5f61b36b81054eaa8ae0fdeb4b
Jose Pedro Magalhaes <jpm@cs.ox.ac.uk>
2013-01-06 13:18:10 +0100

    Now filtering and printing properly

--- Billboard/Transitions.hs
+++ Billboard/Transitions.hs
@@ -5,7 +5,7 @@ module Main ( main ) where
 import qualified Data.Map as M
 
 import HarmTrace.Base.MusicTime (TimedData, getData)
-import HarmTrace.Base.MusicRep (Chord(..), ChordLabel, toMajMinChord)
+import HarmTrace.Base.MusicRep
 
 import Billboard.BillboardParser ( parseBillboard)
 import Billboard.BillboardData ( BillboardData (..), BBChord (..) )
@@ -13,22 +13,47 @@ import Billboard.IOUtils
 
 import System.Environment (getArgs)
 import Control.Monad (foldM)
-import Data.List (intercalate)
+import Data.Monoid (mappend)
+import Data.List (nub, intercalate)
 
 
 type Transitions = M.Map (ChordLabel, ChordLabel) Double
 
+
+allRoots :: [Root]
+-- To keep things simple, we generate a lot of notes, and then simplify them
+allRoots = nub . map simplifyRoot $
+           [ Note m r | m <- [Nothing, Just Sh, Just Fl]
+                      , r <- [C,D,E,F,G,A,B] ]
+
+allChords :: [ChordLabel]
+allChords = [ Chord r sh [] 0 1 | sh <- [Maj,Min,None], r <- allRoots ]
+
+emptyTransitions :: Transitions
+emptyTransitions = M.fromList $
+                   [ ((c1,c2),0.0) | c1 <- allChords, c2 <- allChords ]
+
 -- Get the chords, throw away the rest
 stripData :: BillboardData -> [ChordLabel]
-stripData = map (toMajMinChord . chord . getData) . getSong
+stripData = filter isBad . map (simplify . chord . getData) . getSong where
+  simplify :: ChordLabel -> ChordLabel
+  simplify c = toMajMinChord c { chordRoot = simplifyRoot (chordRoot c)
+                               , chordAdditions = []
+                               , getLoc = 0, duration = 1 }
+  
+  isBad :: ChordLabel -> Bool
+  isBad (Chord (Note _ N) _ _ _ _) = False
+  isBad (Chord (Note _ X) _ _ _ _) = False
+  isBad _                          = True
 
 -- Process all files in the dir
 statsAll :: FilePath -> IO Transitions
 statsAll fp = do files <- getBBFiles fp
-                 ts <- foldM doFiles M.empty files
-                 -- now show probability instead of counts
-                 let nrTs = M.foldr (+) 0 ts
-                 return $ M.map (/ nrTs) ts where
+                 ts <- foldM doFiles emptyTransitions files
+                 -- uncomment to show probability instead of counts
+                 -- let nrTs = M.foldr (+) 0 ts
+                 -- return $ M.map (/ nrTs) ts where
+                 return ts where
   doFiles ts (d,_) = readFile d >>= return . statsOne . fst . parseBillboard where
     statsOne :: BillboardData -> Transitions
     statsOne = doChords . stripData
@@ -38,19 +63,23 @@ statsAll fp = do files <- getBBFiles fp
     doChords _         = ts
 
 instance (Ord a) => Ord (Chord a) where
-  compare a b = compare (chordRoot a) (chordRoot b)
+  compare a b = compare (chordRoot a) (chordRoot b) `mappend`
+                compare (chordShorthand a) (chordShorthand b)
 
+-- Pretty-print the transition matrix
 printTransitions :: Transitions -> String
-printTransitions ts = intercalate "," [ show c | ((c, _), _) <- M.toAscList ts ] 
+printTransitions ts = "," ++ 
+                      intercalate "," [ show c1 | ((c1, c2), _) <- M.toAscList ts
+                                                , c1 == c2 ] 
                       ++ "\n" ++ concat
                       [ show c1 ++ ","
                         ++ intercalate ","
                            [ show p
                            | ((c1', c2'), p) <- M.toAscList ts, c1 == c1' ] ++ "\n"
-                      | ((c1, _), _) <- M.toAscList ts ]
+                      | ((c1, c2), _) <- M.toAscList ts, c1 == c2 ]
   
 main :: IO ()
 main = do (path:_) <- getArgs
           ts <- statsAll path
-          print ts
           -- putStrLn $ printTransitions ts
+          print (M.toAscList ts)
