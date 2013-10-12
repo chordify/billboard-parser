e1048a724ee9c766b119682b6b4ddfb389b5898d
Bas de Haas <wbdehaas@xs4all.nl>
2013-10-12 16:26:58 +0200

    minor fix

--- Main.hs
+++ Main.hs
@@ -55,8 +55,7 @@ myArgs = [
                  argAbbr  = Just 'm',
                  argName  = Just "mode",
                  argData  = argDataRequired "mode" ArgtypeString,
-                 argDesc  = 
-                   breakLn 53 "The billboard-parser features five modes ( parse | mirex | test | full | result | debug): With the first mode, parse, one or more pieces are parsed, and the piece is printed to the user in a way that resembles the original. If a piece cannot be parsed correctly, the parser will inform the user which part of the input could not be parsed, and where this occurred exactly in the file. The second mode, mirex, converts billboard pieces into the format typically used in MIREX evaluations: onset <space> offset <space> chordlabel. In MIREX mode, the chord labels are also truncated to the triad shorthands maj, min, dim, aug, sus2, and sus4; all additional chord extensions are removed. We truncate a chord by expanding it to its corresponding list of intervals, and analyse the components, if any, that are a second, third, fourth, or fifth above the root. The third mode, full, uses the same format as the mirex mode, but it prints the full chordlabel as found in the data. The fourth mode, test, executes a series of unit tests that flag chords with an unexpected duration. The mode result will output the results described in the technical report of this parser. Finally, debug, is verbose mode for debugging the parser."
+                 argDesc  = breakLn 53 "The billboard-parser features five modes ( parse | mirex | test | full | result | debug): With the first mode, parse, one or more pieces are parsed, and the piece is printed to the user in a way that resembles the original. If a piece cannot be parsed correctly, the parser will inform the user which part of the input could not be parsed, and where this occurred exactly in the file. The second mode, mirex, converts billboard pieces into the format typically used in MIREX evaluations: onset <space> offset <space> chordlabel. In MIREX mode, the chord labels are also truncated to the triad shorthands maj, min, dim, aug, sus2, and sus4; all additional chord extensions are removed. We truncate a chord by expanding it to its corresponding list of intervals, and analyse the components, if any, that are a second, third, fourth, or fifth above the root. The third mode, full, uses the same format as the mirex mode, but it prints the full chordlabel as found in the data. The fourth mode, test, executes a series of unit tests that flag chords with an unexpected duration. The mode result will output the results described in the technical report of this parser. Finally, debug, is verbose mode for debugging the parser."
                }
         ,  Arg { argIndex = OutDir,
                  argAbbr  = Just 'o',
@@ -94,7 +93,7 @@ myArgs = [
                }
          ]
 
--- breaks a long string into lines wh
+-- fixes the indentation of the manual 
 breakLn :: Int -> String -> String         
 breakLn l = intercalate ('\n' : replicate (79-l) ' ')
           . map (intercalate " ") . foldr step [] . words where
